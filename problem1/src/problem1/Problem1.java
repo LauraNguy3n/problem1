44058135641155099769889ade67dfdc3c22bcee
laura <you@example.com>
2024-08-21 12:13:05 -0500

    hi

---
+++ problem1/src/problem1/Problem1.java
@@ -0,0 +1,30 @@
+package problem1;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// Data Types int, string, double, boolean, 
+		
+		int a = 14;
+		System.out.println(a);
+		
+		String e = "hello";
+		System.out.println(e);
+		
+		double i = 23.134;
+		System.out.println(i);
+		
+		boolean o = false;
+		System.out.println(o);
+		
+		float t = 1.23f;
+		System.out.println(t);
+		
+		
+		// we didn't learn long, or at least i forgot. and i can't get float to work.
+		
+		
+
+	}
+
+}
